b9405446dc9f5539de8a79b750e66c9e5184cfdc
Vrush2004 <vrushalichaube@gmail.com>
2024-10-18 21:35:49 +0530

    DSA

--- DSA_Imp/RecursionAndBacktracking/TargetSumSubset.java
+++ DSA_Imp/RecursionAndBacktracking/TargetSumSubset.java
@@ -14,45 +14,26 @@ package DSA_Imp.RecursionAndBacktracking;
 import java.util.ArrayList;
 
 public class TargetSumSubset {
-    public static void main(String[] args){
-        int[] arr = {10, 20, 30, 40, 50};
-        permutations(arr, 0, new boolean[arr.length] , new ArrayList<>());
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};  
+        int target = 60;                  
+        
+        System.out.println("Subsets whose sum equals " + target + ":");
+        tss(arr, 0, target, new ArrayList<>());
     }
 
-    public static void permutations(int[] arr, int pos, boolean[] selected, ArrayList<Integer> ans){
-        if(pos == arr.length){
-            System.out.println(ans);
-            return;
-        }
-
-        for(int i = 0; i < arr.length; i++){
-            if(selected[i] == false){
-                selected[i] = true;
-                ans.add(arr[i]);
-                permutations(arr, pos + 1, selected, ans);
-                selected[i] = false;
-                ans.remove(ans.size() - 1);
+    public static void tss(int[] arr, int i, int target, ArrayList<Integer> ans) {
+        if (i == arr.length) {  
+            if (target == 0) {  
+                System.out.println(ans);
             }
-        }
-    }
-
-    public static void tss(int[] arr, int i, int target, ArrayList<Integer> ans){
-        if(target < 0){
             return;
         }
 
-        if(i == arr.length){
-            if(target == 0)
-            System.out.println(ans);
-            return;
-        }
-
-        // select ith element
         ans.add(arr[i]);
-        tss(arr, i+1, target - arr[i], ans);
-        ans.remove(ans.size() - 1);
+        tss(arr, i + 1, target - arr[i], ans); 
+        ans.remove(ans.size() - 1);  
 
-        // reject ith element
-        tss(arr, i+1, target, ans);
+        tss(arr, i + 1, target, ans); 
     }
 }
