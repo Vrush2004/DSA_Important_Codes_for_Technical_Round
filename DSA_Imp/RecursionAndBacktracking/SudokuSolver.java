da8b87f427533ff9165f6df5640f4ce2734b2f45
Vrush2004 <vrushalichaube@gmail.com>
2024-10-19 21:06:23 +0530

    DSA

--- DSA_Imp/RecursionAndBacktracking/SudokuSolver.java
+++ DSA_Imp/RecursionAndBacktracking/SudokuSolver.java
@@ -1,10 +1,30 @@
+// Time Complexity = O(9^m)
+// Space complexity = O(n^2)
+
 package DSA_Imp.RecursionAndBacktracking;
 
 public class SudokuSolver {
+    public static void main(String[] args){
+        char[][] board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        
+        // Solve the Sudoku puzzle and print the result
+        solveSudoku(board);
+    }
+
     public static void solveSudoku(char[][] arr){
         if(sudoku(arr, 0, 0)){
             for(int i =0; i<arr.length; i++){
-                for(int j =0; j<arr[0].length; i++){
+                for(int j =0; j<arr[0].length; j++){
                     System.out.print(arr[i][j] + " ");
                 }
                 System.out.println();
@@ -19,7 +39,7 @@ public class SudokuSolver {
         int nx = y == 8 ? x+1:x;
         int ny = y == 8 ? 0:y+1;
 
-        if(arr[i][j] == '.'){
+        if(arr[x][y] == '.'){
             for(int val = 1; val <= 9; val++){
                 if(canWePlace(arr, x, y, val) == true){
                     arr[x][y] = (char)(val + '0');
@@ -50,5 +70,18 @@ public class SudokuSolver {
                 return false;
             }
         }
+
+        // check the 3*3 grid
+        x = x- (x % 3);
+        y = y- (y % 3);
+
+        for(int i =0; i<2; i++){
+            for(int j = 0; j<2; j++){
+                if(arr[x+i][y+j] == (char)(val + '0')){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
