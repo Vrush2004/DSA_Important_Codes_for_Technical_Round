3a6b7959d4cce9d421825193aa7c161270e7c37b
Vrush2004 <vrushalichaube@gmail.com>
2024-10-19 21:13:21 +0530

    DSA

---
+++ DSA_Imp/DynamicProgramming/FibonacciNum.java
@@ -0,0 +1,11 @@
+// Find nth fibonacci number.
+// Fibonacci numbers form a sequence in which every number is the sum of two preceding numbers.
+// First few fibonacci numbers are - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// Sample input N=10
+// Sample output = 55
+
+package DSA_Imp.DynamicProgramming;
+
+public class FibonacciNum {
+    
+}
