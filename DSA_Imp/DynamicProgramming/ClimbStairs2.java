f2e80b1d23dd77b5876bca4e8a153edf9abbcc15
Vrush2004 <vrushalichaube@gmail.com>
2024-10-24 18:59:29 +0530

    DSA

--- DSA_Imp/DynamicProgramming/ClimbStairs2.java
+++ DSA_Imp/DynamicProgramming/ClimbStairs2.java
@@ -11,8 +11,10 @@ public class ClimbStairs2 {
     public static void main(String[] args){
         int[] arr = {1,2,4,3,6,5,2,7,1};
         System.out.println(stairs(arr, arr.length, new int[arr.length + 1]));
+        System.out.println(stairsTab(arr));
     }
     
+    // Memorization
     public static int stairs(int[] arr, int n, int[] dp){
         if(n == 1){
             return arr[0];
@@ -30,5 +32,17 @@ public class ClimbStairs2 {
         int ans = Math.min(f1,f2) + arr[n-1];
         dp[n] = ans;
         return ans;
+    }
+
+    // Tabulation
+    public static int stairsTab(int[] arr){
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+        dp[1] = arr[0] + arr[1];
+
+        for(int i =2; i<dp.length; i++){
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + arr[i];
+        }
+        return dp[dp.length-1];
     }
 }
---
+++ DSA_Imp/DynamicProgramming/LongestIncSubsequence.java
@@ -0,0 +1,10 @@
+// Given an array.Find the length of longest strictly increasing subsequence.
+// Sample Input 
+// arr = {10,2,,9,5,7,3,60,80,1}
+// Sample output = 5
+
+package DSA_Imp.DynamicProgramming;
+
+public class LongestIncSubsequence {
+    
+}
