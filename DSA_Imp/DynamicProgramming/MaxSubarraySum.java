318e3a1c38def0ddb1840dc676f02df8656091b6
Vrush2004 <vrushalichaube@gmail.com>
2024-10-27 12:36:29 +0530

    DSA

---
+++ DSA_Imp/DynamicProgramming/MaxSubarraySum.java
@@ -0,0 +1,27 @@
+// Give an array of integers. Find the maximum subarray sum.
+// Sample input 
+// arr={2,-3,-2,6,-8,2,1,9,-6,4}
+// Sample output
+// 13
+// Time complexity = O(n)
+// space complexity = O(1)
+
+package DSA_Imp.DynamicProgramming;
+
+public class MaxSubarraySum {
+    public static void main(String[] args){
+        int[] arr = {2,-3,-2,6,-8,2,1,9,-6,4};
+        System.out.println(maxsumsubarr(arr));
+    }
+
+    public static int maxsumsubarr(int[] arr){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i =0; i<arr.length; i++){
+            currSum = Math.max(arr[i], currSum + arr[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+}
