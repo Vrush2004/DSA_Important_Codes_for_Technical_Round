c1932ef8aca3f01d4c7125fe6bc559abcabcb7d0
Vrush2004 <vrushalichaube@gmail.com>
2024-10-27 13:45:12 +0530

    DSA

--- DSA_Imp/DynamicProgramming/KConcatenation.java
+++ DSA_Imp/DynamicProgramming/KConcatenation.java
@@ -17,16 +17,28 @@ public class KConcatenation {
     }
 
     public static int main(int[] arr, int k){
-
+        if(k == 1){
+            return kadanes(arr, 1);
+        }else{
+            int sum = 0;
+            for(int i=0; i<arr.length; i++){
+                sum += arr[i];
+            }
+            if(sum > 0){
+                return (k-2)*sum + kadanes(arr, 2);
+            }else{
+                return kadanes(arr, 2);
+            }
+        }
     }
 
-    public static int kadanes(int[] arr){
+    public static int kadanes(int[] arr, int c){
         int currSum =0;
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i< arr.length; i++){
             currSum = Math.max(currSum+arr[i], arr[i]);
-            maxSum = Math.max(maxSum, currSum)
+            maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
     }
