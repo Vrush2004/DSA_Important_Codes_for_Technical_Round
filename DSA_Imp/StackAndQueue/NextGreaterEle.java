633729330f0ebca1f3673a8d0354e6158e5e26c9
Vrush2004 <vrushalichaube@gmail.com>
2024-10-30 12:26:43 +0530

    DSA

--- DSA_Imp/StackAndQueue/NextGreaterEle.java
+++ DSA_Imp/StackAndQueue/NextGreaterEle.java
@@ -20,6 +20,26 @@ public class NextGreaterEle {
         System.out.println();
     }
 
+    public static int[] greaterEle2(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        st.push(0);
+
+        for(int i = 1; i<arr.length; i++){
+            // Pop all smaller elements
+            // for all these smaller elements, nge is current element
+            while(st.size() > 0 && arr[st.peek()] < arr[i]){
+                ans[st.peek()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (st.size() > 0) {
+            ans[st.pop()] = -1;
+        }
+        return ans;
+    }
+
     public static int[] greaterEle(int[] arr){
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[arr.length];
