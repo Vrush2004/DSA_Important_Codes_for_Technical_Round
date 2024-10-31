709d1844662662e41bfd24c4017e5ce7d859e831
Vrush2004 <vrushalichaube@gmail.com>
2024-10-31 10:09:16 +0530

    DSA

--- DSA_Imp/LinkedList/PalindromeOrNot.java
+++ DSA_Imp/LinkedList/PalindromeOrNot.java
@@ -45,4 +45,57 @@ public class PalindromeOrNot {
             temp = temp.next;
         }
     }
+
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+
+            // move prev and curr
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static int middleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.val;
+    }
+
+    public static boolean isPalindrome(ListNode head){
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = reverse(slow.next);
+        ListNode first = head;
+
+        while(first != null && second != null){
+            if(first.val != second.val){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
 }
