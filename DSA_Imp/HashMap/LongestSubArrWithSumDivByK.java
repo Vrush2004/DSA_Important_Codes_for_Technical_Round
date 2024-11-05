e3bca1d23b5dbd5719f41eaec47c6388dc7a9f64
Vrush2004 <vrushalichaube@gmail.com>
2024-11-05 21:27:56 +0530

    DSA

---
+++ DSA_Imp/HashMap/LongestSubArrWithSumDivByK.java
@@ -0,0 +1,14 @@
+// Given an array of size N and an integer K. Find the longest subarray's length with the sum of elements divisible by K.
+// Sample input 
+// arr ={2, 7, 6, 1, 4, 5}, k=3
+// Sample output = 4
+// Time complexity = O(n)
+// Space complexity = O(n)
+
+package DSA_Imp.HashMap;
+
+public class LongestSubArrWithSumDivByK {
+    public static void main(String[] args){
+        
+    }
+}
