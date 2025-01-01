5c0639f641c751269cb6f3b210092784aa7aea0b
Vrush2004 <vrushalichaube@gmail.com>
2025-01-01 20:55:17 +0530

    singly linked list

--- LinkedList/SinglyLinkedList/InsertionAtEnd.java
+++ LinkedList/SinglyLinkedList/InsertionAtEnd.java
@@ -14,4 +14,58 @@ public class InsertionAtEnd {
             next = null;
         }
     }
+    public void insertEnd(int data)
+    {
+        // Creating newNode memory & assigning data value
+        Node newNode = new Node(data);
+
+        // if Linked List was empty, entering first node
+        if(head==null)
+        {
+            head = newNode;
+            System.out.println(newNode.data + " inserted");
+            return;
+        }
+
+        // required to traverse the Linked List
+        Node temp = head;
+
+        // traverse till the last node in Linked List
+        while(temp.next!=null)
+            temp = temp.next;
+
+        // assigning the current last node's next to this newNode
+        // thus newNode becomes the last node in Linked List
+        temp.next = newNode;
+        System.out.println(newNode.data + " inserted");
+
+    }
+
+
+    public void display()
+    {
+        System.out.println();
+        Node node = head;
+        //as linked list will end when Node reaches Null
+        while(node!=null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        InsertionAtEnd ll = new InsertionAtEnd();
+
+        ll.insertEnd(50);
+        ll.insertEnd(40);
+        ll.insertEnd(30);
+        ll.insertEnd(20);
+        ll.insertEnd(10);
+
+        ll.display();
+
+    }
 }
