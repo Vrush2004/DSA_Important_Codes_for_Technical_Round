28960f82317dc560e2fd0e5ff0afd3db3f6102a9
Vrush2004 <vrushalichaube@gmail.com>
2025-01-05 20:57:20 +0530

    DSA

--- LinkedList/SinglyLinkedList/LinkedListIsPalindrome.java
+++ LinkedList/SinglyLinkedList/LinkedListIsPalindrome.java
@@ -68,4 +68,74 @@ public class LinkedListIsPalindrome {
         }
         System.out.println ("END");
     }
+
+    // Function to add a node in beginning of linked list
+    public void addFirst (int item)
+    {
+        // Create a temp node which points to head
+        Node temp = new Node (item, head);
+
+        // If linked list is empty, temp is the head and tail node both
+        if (this.size == 0)
+        {
+            this.head = this.tail = temp;
+        }
+
+        // else set the head such that it now points to temp node
+        else
+        {
+            this.head = temp;
+        }
+        this.size++;
+    }
+
+    public boolean isPalindrome ()
+    {
+        HeapMover start = new HeapMover ();
+        start.node = this.head;
+        return this.isPalindrome (start, this.head, 0);
+    }
+
+    //Function to check whether linked list is palindrome or not
+    private boolean isPalindrome (HeapMover start, Node end, int floor)
+    {
+        //Base case is when we reach end of linked list
+        if (end == null)
+        {
+            return true;
+        }
+
+        //Recursive calls
+        boolean rv = this.isPalindrome (start, end.next, floor + 1);
+
+        //If any recursive call results in false then return false
+        if (rv == false)
+        {
+            return false;
+        }
+
+        //Till floor is greater than 1/2 * size of linked list
+        if (floor >= this.size () / 2)
+        {
+            //If data of start node and end node is not same then it is not palindrome
+            if (start.node.data != end.data)
+            {
+                return false;
+            }
+
+            //Change start node so that it now points to the next node
+            else
+            {
+                start.node = start.node.next;
+                return true;
+            }
+        }
+        return rv;
+    }
+
+    //Class to keep a node in the heap
+    private class HeapMover
+    {
+        Node node;
+    }
 }
