d8b00a3ea7dcd89e60cadbc1d2744bda27849584
Vrush2004 <vrushalichaube@gmail.com>
2025-01-11 21:15:04 +0530

    DSA

--- LinkedList/DoublyLinkedList/InsertAtNthNode.java
+++ LinkedList/DoublyLinkedList/InsertAtNthNode.java
@@ -19,4 +19,64 @@ class InsertAtNthNode {
             prev = null;
         }
     }
+
+    public void insertBeginning(int data) {
+        // Creating newNode memory & assigning data value
+        Node freshNode = new Node(data);
+
+        freshNode.next = head;
+        freshNode.prev = null;
+
+        // if DLL had already >=1 nodes
+        if (head != null)
+            head.prev = freshNode;
+
+        // changing head to this
+        head = freshNode;
+    }
+
+    public void insertEnd(int data) {
+        // Creating newNode memory & assigning data value
+        Node freshNode = new Node(data);
+
+        // assign data
+        // since this will be the last node its next will be NULL
+        freshNode.next = null;
+
+        //if we are entering the first node
+        if (head == null) {
+            head = freshNode;
+            freshNode.prev = null;
+            return;
+        }
+
+        Node last = head;
+
+        // traverse to the current last node
+        while (last.next != null)
+            last = last.next;
+
+        // assign current last node's next to this new node
+        // assign new node's previous to this last node
+        last.next = freshNode;
+        freshNode.prev = last;
+        // new_node becomes the last node now
+
+    }
+
+    public static void main(String args[])
+    {
+        InsertAtNthNode doublylist = new InsertAtNthNode();
+
+        doublylist.insertBeginning(3);
+        doublylist.insertBeginning(2);
+        doublylist.insertBeginning(1);
+        doublylist.insertBeginning(4);
+        doublylist.insertBeginning(7);
+
+        //Inserts after 4th position
+        doublylist.insertAfterPosition(4,5);
+
+        doublylist.printList();
+    }
 }
