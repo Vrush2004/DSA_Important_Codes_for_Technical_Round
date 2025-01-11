01b2ffde65877bb88bdcf72978dfe7f0c93e4e13
Vrush2004 <vrushalichaube@gmail.com>
2025-01-11 21:15:30 +0530

    DSA

--- LinkedList/DoublyLinkedList/InsertAtNthNode.java
+++ LinkedList/DoublyLinkedList/InsertAtNthNode.java
@@ -64,6 +64,86 @@ class InsertAtNthNode {
 
     }
 
+    public void insertAfterPosition(int n, int data) {
+        int len = getLength(head);
+
+        // if insertion position is 0 means entering at start
+        if (n == 0) {
+            insertBeginning(data);
+            return;
+        }
+        // means inserting after last item
+        if (n == len) {
+            insertEnd(data);
+            return;
+        }
+
+        // else insertion will happen somewhere in the middle
+
+
+        if (n < 1 || len < n) System.out.println("Invalid position");
+        else { Node freshNode = new Node(data);
+            // can remove null assignments also (constructor takes care of these)
+            // added just for explanation purpose
+
+            freshNode.next = null;
+
+            freshNode.prev = null;
+            // nthNode used to traverse the Linked List
+
+            Node nthNode = head;
+
+            // traverse till the nth node
+
+            while (--n > 0) {
+                nthNode = nthNode.next;
+            }
+
+            Node nextNode = nthNode.next; // (n+1)th node
+
+            // assigning (n+1)th node's previous to this new node
+            nextNode.prev = freshNode;
+
+            // new_node's next assigned to (n+1)th node
+            freshNode.next = nextNode;
+            // new_node's previous assigned to nth node
+            freshNode.prev = nthNode;
+
+            // assign nth node's next to new_node
+            nthNode.next = freshNode;
+        }
+    }
+
+    public void printList() {
+        Node node = head;
+        Node end = null;
+        //as linked list will end when Node reaches Null
+
+        System.out.print("\nIn forward: ");
+        while (node != null) {
+            System.out.print(node.data + " ");
+            end = node;
+            node = node.next;
+        }
+        System.out.print("\nIn backward: ");
+
+        while (end != null) {
+            System.out.print(end.data + " ");
+            end = end.prev;
+        }
+        System.out.println();
+    }
+
+    // required for insertPosition() method
+    public int getLength(Node node) {
+        int size = 0;
+        // traverse to the last node each time incrementing the size
+        while (node != null) {
+            node = node.next;
+            size++;
+        }
+        return size;
+    }
     public static void main(String args[])
     {
         InsertAtNthNode doublylist = new InsertAtNthNode();
