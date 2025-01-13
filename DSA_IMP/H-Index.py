4301001027503204f257004bcab7e9e37a0f55ef
Vrush2004 <vrushalichaube@gmail.com>
2025-01-13 19:12:29 +0530

    DSA

---
+++ DSA_IMP/H-Index.py
@@ -0,0 +1,19 @@
+# Input: citations = [3,0,6,1,5]
+# Output: 3
+# Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+# Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+class Solution:
+    def hIndex(self, citations: list[int]) -> int:
+        # Sort citations in descending order
+        citations.sort(reverse=True)
+        
+        h_index = 0
+        for i, citation in enumerate(citations):
+            # Check if the current citation count is greater than or equal to the current index + 1
+            if citation >= i + 1:
+                h_index = i + 1
+            else:
+                break
+        
+        return h_index
