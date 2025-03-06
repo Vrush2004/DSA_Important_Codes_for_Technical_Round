d17011942b122e5d4eb1773f368832915d9ef20a
Vrush2004 <vrushalichaube@gmail.com>
2025-03-06 10:34:17 +0530

    DSA

---
+++ BinaryTreeGeneral/BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,42 @@
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [[3],[20,9],[15,7]]
+
+from collections import deque
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+        left_to_right = True  # Flag to indicate traversal direction
+
+        while queue:
+            level_size = len(queue)
+            level_nodes = deque()
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                if left_to_right:
+                    level_nodes.append(node.val)
+                else:
+                    level_nodes.appendleft(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(list(level_nodes))
+            left_to_right = not left_to_right  # Toggle direction
+
+        return result
