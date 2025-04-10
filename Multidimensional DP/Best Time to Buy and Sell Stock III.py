bc0d1fe9bb2475ffc47b5e7318934ee244d9c2bc
Vrush2004 <vrushalichaube@gmail.com>
2025-04-10 10:36:53 +0530

    DSA

---
+++ Multidimensional DP/Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,19 @@
+# Input: prices = [3,3,5,0,0,3,1,4]
+# Output: 6
+# Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+# Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1 = buy2 = float('-inf')
+        sell1 = sell2 = 0
+
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+
+        return sell2
